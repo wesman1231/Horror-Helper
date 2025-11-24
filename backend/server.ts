4fbd74a4ce06cb433650f37993066224e69776c0
Wesley <wesquirk@gmail.com>
2025-11-23 23:30:17 -0800

    backend implemented along with some front end changes, getAllMovies used to get all horror movies from TMDB to my local DB

---
+++ backend/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import searchRoutes from './routes/searchRoutes.ts';
+
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use('/api/search', searchRoutes);
+app.use('/api/search/', searchRoutes);
+
+app.listen(3000, () => {
+    console.log("IT'S ALIVE AT http://localhost:3000");
+});
--- frontend/src/pages/movies.tsx
+++ frontend/src/pages/movies.tsx
@@ -1,19 +1,38 @@
 import styles from './movies.module.css';
 import MovieCard from '../UI-Elements/movieCard';
-
+import { useState } from 'react';
 
 export default function Movies(){
 
+    const [searchValue, setSearchValue] = useState('');
+
+    function handleInput(event: any){
+        setSearchValue(event.target.value);
+    }
+
+    async function search(){
+        console.log("Button clicked!", searchValue);
+        const formatSearch = searchValue.replaceAll(' ', "+");
+        console.log(formatSearch);
+        try{
+            const request = await fetch(`http://localhost:3000/api/search/movies?query=${formatSearch}`);
+            const response = await request.json();
+            console.log(response);
+        } catch(error){
+            console.error('Error searching: ', error);
+        }
+    }
 
     return(
         <>
             <div className={styles.searchBarContainer}>
-                <input type='text' id="search-bar"className={styles.searchBar} placeholder='Enter a film title, actor, director, etc.'></input>
+                <input type='text' id="search-bar"className={styles.searchBar} value={searchValue} onChange={handleInput} placeholder='Enter a film title, actor, director, etc.'></input>
+                <button type='button' onClick={search}>search</button>
             </div>
             <div className={styles.resultsContainer}>
                 <MovieCard/>
             </div>
-            
         </>
     );
 }
+
