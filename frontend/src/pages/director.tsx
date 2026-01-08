36dded6bc6ccc83b00555a491ffa1bbfbab4b9d1
Wesley <wesquirk@gmail.com>
2026-01-07 19:45:48 -0800

    Updated director page, now includes director photo, birthday, bio, death date, and all of their films listed in order of release date. DirectorDataController is now function based instead of class based.

--- frontend/src/pages/director.tsx
+++ frontend/src/pages/director.tsx
@@ -2,20 +2,29 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import type { Movie } from "../UI-Elements/movieCard";
 import MovieCard from "../UI-Elements/movieCard";
+import styles from "../pages/pages_css/director.module.css";
 
 export default function Director(){
 
+
     const { directorName } = useParams();
     const [directorMovies, setDirectorMovies] = useState<Movie[]>([]);
+    const [directorImage, setDirectorImage] = useState<string>();
+    const [directorBirthday, setDirectorBirthday] = useState<string>();
+    const [directorDeathDate, setDirectorDeathDate] = useState<string>();
+    const [directorBio, setDirectorBio] = useState<string>();
 
     useEffect(() => {
         async function getDirectorInfo(){
 
             try{
                 const directorRequest = await fetch(`http://localhost:3000/api/directors/${directorName}`);
-                const directorResponse: Movie[] = await directorRequest.json();
-                setDirectorMovies(directorResponse);
-
+                const directorResponse = await directorRequest.json();
+                setDirectorMovies(directorResponse.movies);
+                setDirectorImage(directorResponse.image);
+                setDirectorBio(directorResponse.profile.biography);
+                setDirectorBirthday(directorResponse.profile.birthday);
+                setDirectorDeathDate(directorResponse.profile.deathday);
             }catch(error){
                 console.error(error);
             }
@@ -25,7 +34,16 @@ export default function Director(){
 
     return(
         <>
-            <ul>
+            <h2 className={styles.directorName}>{directorName?.replaceAll('+', ' ')}</h2>
+            <div className={styles.directorInfoWrapper}>
+                <img src={directorImage} alt={`Image of ${directorName}`}/>
+                <p>Birthday: {`${directorBirthday}`}</p>
+                <p>Biography:</p>
+                <p className={styles.bio}>{`${directorBio}`}</p>
+                <p>{directorDeathDate !== null ? `Death date: ${directorDeathDate}`  : null}</p>
+            </div>
+            <h3 className={styles.filmsHeader}>Films</h3>
+            <ul className={styles.movieList}>
                {directorMovies.map((movie) => 
                 <MovieCard key={movie.tmdbid} tmdbid={movie.tmdbid} title={movie.title} poster={movie.poster} keywords={movie.keywords} director={movie.director} releasedate={movie.releasedate} franchise={movie?.franchise} synopsis={movie.synopsis} cast={movie.cast} />
             )}
