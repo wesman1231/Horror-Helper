55fc2b160f5e6c2ae628015371b288d8a5c84d51
Wesley <wesquirk@gmail.com>
2026-01-11 01:49:00 -0800

    users can now sign up with firebase signup, will add login soon

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -2,8 +2,12 @@ import express from "express";
 import signup from "../controller/authController.ts";
 import { body } from 'express-validator';
 
-const validateEmail = () => body('email').notEmpty().isEmail();
-const validatePassword = () => body('password').notEmpty();
+const validateEmail = () => body('email')
+                            .notEmpty()
+                            .trim()
+                            .isEmail();
+const validatePassword = () => body('password')
+                                .notEmpty();
 const router = express.Router();
 
 router.post('/signup', validateEmail(), validatePassword(), signup);
--- frontend/src/firebase/firebase.ts
+++ frontend/src/firebase/firebase.ts
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import dotenv from 'dotenv';
+dotenv.config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,4 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export default {app: app, auth: auth};
+export {app, auth};
