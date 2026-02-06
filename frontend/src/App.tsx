61a677c3a96c69018d732b1effa869b1e1c03216
Wesley <wesquirk@gmail.com>
2026-02-05 20:47:59 -0800

    all post login routes are now protected by a protected route component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react';
+import type { ReactNode } from 'react';
 
 import Login from './pages/login';
 import LoginRedirect from './pages/loginRedirect';
@@ -15,9 +17,19 @@ import Director from './pages/director';
 import NewReleases from './pages/newReleases';
 import ProvenClassics from './pages/provenClassics';
 import HiddenGems from './pages/hiddenGems';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate  } from "react-router-dom";
 
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { isAuthenticated, isLoading } = useAuth0();
+  
+  if (isLoading) return <div>Loading...</div>;
+  
+  return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
+}
 
 function App() {
   return (
@@ -30,20 +42,52 @@ function App() {
           <Route path='/login' element={<LoginRedirect />}/>
           <Route path="/forgot-password" element={<ResendEmailVerification />} />
           
-          <Route path="/home" element={<Home />}/>
+          <Route path="/home" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }/>
 
           {/*header routes*/}
-          <Route path="/search/:mediaType" element={<MediaSearch />} />
+          <Route path="/search/:mediaType" element={
+            <ProtectedRoute>
+              <MediaSearch />
+            </ProtectedRoute>
+          } />
 
           {/*button routes*/}
-          <Route path="/new-releases" element={<NewReleases />} />
-          <Route path="/proven-classics" element={<ProvenClassics />} />
-          <Route path="/hidden-gems" element={<HiddenGems />} />
+          <Route path="/new-releases" element={
+            <ProtectedRoute>
+              <NewReleases />
+            </ProtectedRoute>
+          } />
+          <Route path="/proven-classics" element={
+            <ProtectedRoute>
+              <ProvenClassics />
+            </ProtectedRoute>
+          } />
+          <Route path="/hidden-gems" element={
+            <ProtectedRoute>
+              <HiddenGems />
+            </ProtectedRoute>
+          } />
           
           {/*media routes*/}
-          <Route path='/movies/:id' element={<MoviePage/>}/>
-          <Route path='/shows/:id' element={<ShowPage/>}/>
-          <Route path='/directors/:directorName' element={<Director />}></Route>
+          <Route path='/movies/:id' element={
+            <ProtectedRoute>
+              <MoviePage/>
+            </ProtectedRoute>
+          }/>
+          <Route path='/shows/:id' element={
+            <ProtectedRoute>
+              <ShowPage/>
+            </ProtectedRoute>
+          }/>
+          <Route path='/directors/:directorName' element={
+            <ProtectedRoute>
+              <Director />
+            </ProtectedRoute>
+          }></Route>
         </Routes>
       </BrowserRouter>
     </>
