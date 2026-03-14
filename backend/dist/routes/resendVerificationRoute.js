8997fd75e2031f42fb8bac0de837838a7b7bf7c3
Wesley <wesquirk@gmail.com>
2026-03-13 22:08:37 -0700

    depencency updates

--- backend/dist/routes/resendVerificationRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { resendVerification } from '../controller/authController.js';
-const router = express.Router();
-router.post('/resend-verification', resendVerification);
-export default router;
