5921b32d9209cfa05a92785612bd35cd3fb4b875
Wesley <wesquirk@gmail.com>
2026-03-13 22:08:37 -0700

    depencency updates

--- backend/dist/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { fetchMovieInfo } from '../controller/movieDataController.js';
-import { fetchNewReleases } from '../controller/movieDataController.js';
-import { fetchFranchiseInfo } from '../controller/movieDataController.js';
-const router = express.Router();
-router.get('/new-releases', fetchNewReleases);
-router.get('/:id', fetchMovieInfo);
-router.get('/franchises/:franchiseName', fetchFranchiseInfo);
-export default router;
--- backend/dist/routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { checkJWT } from "../middleware/checkJWT.js";
-import { postReview, getReviews } from "../controller/reviewController.js";
-const router = express.Router();
-router.get('/get', getReviews);
-router.post('/post', checkJWT, postReview);
-export default router;
