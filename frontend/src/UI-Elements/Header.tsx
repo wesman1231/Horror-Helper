72f953c91d7e7d21103a0058c9c79bad8e5b685a
Wesley <wesquirk@gmail.com>
2026-04-01 18:23:04 -0700

    updated home header link to direct to the root url

--- frontend/src/UI-Elements/Header.tsx
+++ frontend/src/UI-Elements/Header.tsx
@@ -9,7 +9,7 @@ export default function Header(){
             <header className={styles.header}>
                 <h1>Horror Helper</h1>
                 <nav>
-                    <NavLink to = '/home' className={styles.headerLink}>Home</NavLink>
+                    <NavLink to = '/' className={styles.headerLink}>Home</NavLink>
                     <NavLink to = '/search/movies' className={styles.headerLink}>Movies</NavLink>
                     <NavLink to = '/search/shows' className={styles.headerLink}>Television</NavLink>
                     <SignInOrOutButton />
